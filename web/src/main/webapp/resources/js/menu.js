2820e1f2398980ad4cec04969e8e4e49408a1efb
jianlai2007 <jianlai2007@qq.com>
2017-06-06 17:04:25 +0800

    搭建基础框架

--- web/src/main/webapp/resources/js/menu.js
+++ web/src/main/webapp/resources/js/menu.js
@@ -1,14 +1,104 @@
 var menu = {
-    //封装菜单相关的url
-    URL:{
-        menusUrl:function () {
+    URL: {
+        menuList: function () {
             return "/menu/list";
+        },
+        mainMenus: function () {
+            return "/menu/mainMenus";
+        },
+        resourceList: function () {
+            return "/resource/list";
+        },
+        add: function () {
+            return "/menu/add";
         }
     },
-    getMenus:function () {
-        var menuUrl = menu.URL.menusUrl();
-        $.get(menuUrl,function (result) {
-            console.log(result)
+
+    initParentNodeSelect: function () {
+        $.get(menu.URL.mainMenus()).success(function (response) {
+            $('#parentGuid').empty();
+            $('#parentGuid').append("<option value=''></option>");
+            var mainMenus = response['data'];
+            for (var i = 0; i < mainMenus.length; ++i) {
+                $('#parentGuid').append('<option value="' + mainMenus[i].guid + '">' + mainMenus[i].menuName + '</option>');
+            }
+        })
+    },
+
+    initChildNodeSelect: function () {
+        $.get(menu.URL.resourceList()).success(function (response) {
+            $('#resourceGuid').empty();
+            $('#resourceGuid').append("<option value=''></option>");
+            var resources = response['data'];
+            for (var j = 0; j < resources.length; ++j) {
+                $('#resourceGuid').append('<option value="' + resources[j].guid + '">' +
+                    '(' + resources[j].description + ')' +
+                    resources[j].resource
+                    + '</option>');
+            }
+
+        })
+    },
+
+    addMenu: function () {
+
+        var parentGuid = $("#parentGuid").val();
+        var resourceGuid = $("#resourceGuid").val();
+        var menuName = $("#menuName").val();
+        var menuIndex = $("#menuIndex").val();
+        var menuType = $("#menuType").val();
+        var iconClass = $("#iconClass").val();
+
+        var menuParam = {
+            parentGuid: parentGuid,
+            resourceGuid: resourceGuid,
+            menuName: menuName,
+            menuIndex: menuIndex,
+            menuType: menuType,
+            iconClass: iconClass
+        };//拼装成json格式
+
+        $.post(menu.URL.add(), menuParam).success(function (response) {
+            console.log(response)
         });
     }
 }
+
+var setting = {
+    view: {
+        showIcon: false,
+        showLine: true
+    },
+    check: {
+        enable: true,
+        chkStyle: "checkbox"
+    },
+    data: {
+        key: {
+            name: "menuName"
+        },
+        simpleData: {
+            enable: true,
+            idKey: "guid",
+            pIdKey: "parentGuid"
+        }
+    },
+    async: {
+        enable: true,
+        url: menu.URL.menuList(),
+        type: "get"
+    },
+    callback: {
+        onAsyncSuccess: function (event, treeId, msg) {
+            var zTree = $.fn.zTree.getZTreeObj(treeId);
+            zTree.expandAll(true);
+        }
+    }
+};
+
+var menuTree = {
+    initZtree: function () {
+        $.fn.zTree.init($("#menuTree"), setting);
+    }
+}
+
