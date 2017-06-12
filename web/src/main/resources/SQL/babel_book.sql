5b4dd800a2ef60969774e7beabf797a2cba0a9cc
jianlai2007 <jianlai2007@qq.com>
2017-06-12 22:40:45 +0800

    搭建基础框架

--- web/src/main/resources/SQL/babel_book.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO babel.book (name, number) VALUES ('Java程序设计', 3);
-INSERT INTO babel.book (name, number) VALUES ('数据结构', 9);
-INSERT INTO babel.book (name, number) VALUES ('设计模式', 10);
-INSERT INTO babel.book (name, number) VALUES ('编译原理', 10);
--- web/src/main/resources/SQL/babel_menu.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO babel.menu (guid, menu_name, resource_guid, parent_guid, menu_index, icon_class) VALUES ('435186ec62494411bae176128fd13d31', '股票指数', 'c49910119a444f56a6cef94c782ca71a', 'fc4bf1ba96fb436086e194eb2a39e7b8', 1, '');
-INSERT INTO babel.menu (guid, menu_name, resource_guid, parent_guid, menu_index, icon_class) VALUES ('4e5d222772ad414fbd490e503d5d704e', '系统管理', '', '', 2, 'fa fa-gear');
-INSERT INTO babel.menu (guid, menu_name, resource_guid, parent_guid, menu_index, icon_class) VALUES ('64675a31a90d40b1bbf4da78a524b295', '权限', 'c4f014399c254929ac0888afd32ce671', '4e5d222772ad414fbd490e503d5d704e', 2, '');
-INSERT INTO babel.menu (guid, menu_name, resource_guid, parent_guid, menu_index, icon_class) VALUES ('890e6bdbf20248ada333b9a9c5628f99', '角色', 'b122de76ee9542c0b2aff0f91bdd8d14', '4e5d222772ad414fbd490e503d5d704e', 3, '');
-INSERT INTO babel.menu (guid, menu_name, resource_guid, parent_guid, menu_index, icon_class) VALUES ('a769060537c04866bd9dac0dd67ee2bc', '菜单', 'c21c87cadade4e8cb9a5b37b882421ce', '4e5d222772ad414fbd490e503d5d704e', 1, 'menu');
-INSERT INTO babel.menu (guid, menu_name, resource_guid, parent_guid, menu_index, icon_class) VALUES ('f6464799ba9b4b0c9309dd950b363a31', '用户', '5d2b0d735aab4f6e886c54c56535ab64', '4e5d222772ad414fbd490e503d5d704e', 4, '');
-INSERT INTO babel.menu (guid, menu_name, resource_guid, parent_guid, menu_index, icon_class) VALUES ('fc4bf1ba96fb436086e194eb2a39e7b8', '股票分析', '', '', 1, 'fa fa-line-chart');
