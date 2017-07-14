aeac855c4ac155b35fae8b600d538a2a08f563a7
jianlai2007 <jianlai2007@qq.com>
2017-07-14 17:13:54 +0800

    搭建架构

--- web/src/main/java/com/babel/web/system/service/impl/RoleServiceImpl.java
+++ web/src/main/java/com/babel/web/system/service/impl/RoleServiceImpl.java
@@ -4,12 +4,14 @@ import com.babel.web.system.dao.RoleDao;
 import com.babel.web.system.entity.Role;
 import com.babel.web.system.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by allen on 2017/5/31.
  */
+@Service
 public class RoleServiceImpl implements RoleService {
 
     @Autowired
